ed738284b8b3248469d53c0531919741012877d3
irajesicajanuaris <jjiraaa1@gmail.com>
2024-10-11 22:10:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/testpraktikum1/TipeData.kt
@@ -0,0 +1,45 @@
+package com.example.testpraktikum1
+
+fun main() {
+
+    //Byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    // short
+    val shortValue: Short = 20
+    println("Short: $shortValue")
+
+    //integer
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+    //long
+    val longValue: Long = 40
+    println("Long: $longValue")
+
+    //float
+    val floatValue: Float = 50.6f
+    println("FloatValue: $floatValue")
+
+    //Double
+    val doubleValue: Double = 60.6
+    println("Double: $doubleValue")
+
+    //char
+    val charValue: Char = 'A'
+    println("char: $charValue")
+
+    //Boolean
+    val booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+    //string
+    var stringValue : String = "Hello, world!"
+    println("string : $stringValue")
+
+    stringValue = "Hello, Kotlin"
+    println("string : $stringValue")
+
+
+}

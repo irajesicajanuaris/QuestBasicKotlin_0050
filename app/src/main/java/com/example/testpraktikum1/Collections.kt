2f12cb25149c13db0b3957384f120457c8021689
irajesicajanuaris <jjiraaa1@gmail.com>
2024-10-11 23:26:40 +0700

    menambahkan contoh set read-only dan set mutable

--- app/src/main/java/com/example/testpraktikum1/Collections.kt
+++ app/src/main/java/com/example/testpraktikum1/Collections.kt
@@ -39,3 +39,23 @@ fun ContohList() {
     val shapeLocked: List<String> = shape
     println(shapeLocked)
 }
+
+// set adalah kumpulan data yang memiliki posisi atau urutan dan hanya menyimpan data unik(tidak ada duplikasi)
+// set bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
+// set bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// set read only menggunakan setOf
+// set mutable menggunakan mutableSetOf
+
+fun ContohSet(){
+    println()
+    println("=== Set ===")
+
+    //Set Read-Only
+    val readOnlyAbjad = setOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    //Set Mutable
+    val shape: MutableSet<String> = mutableSetOf("Circle", "Square", "Triangle")
+    println(shape)
+
+}

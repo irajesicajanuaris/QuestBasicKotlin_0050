07c0de19905be52b86e0bcf0991d873d808f2c8e
irajesicajanuaris <jjiraaa1@gmail.com>
2024-10-11 23:05:20 +0700

    menambahkan fungsi ContohList

---
+++ app/src/main/java/com/example/testpraktikum1/Collections.kt
@@ -0,0 +1,11 @@
+package com.example.testpraktikum1
+
+//list adalah kumpulan data yang disusun secara berurutan
+//list bersifat ordered, artinya data yang dimasukkan akan memiliki posisi sesuai urutan
+//list bersidat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+//list Read-Only menggunakan ListOf
+//list Mutable menggunakan mutableListOf
+
+fun ContohList() {
+    println("== List ==")
+}
